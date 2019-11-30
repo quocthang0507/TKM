0902fc685afed54a93e3db811fa5232b98e4a743
La Quốc Thắng <quocthang0507@gmail.com>
2019-11-30 10:07:14 +0700

    Upload new project and BaoCao.docx

---
+++ Lab/Lab 2/Decorator/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Decorator
+{
+	class Program
+	{
+		static void Main()
+		{
+			ConcreteComponent c = new ConcreteComponent();
+			ConcreteDecoratorA d1 = new ConcreteDecoratorA();
+			ConcreteDecoratorB d2 = new ConcreteDecoratorB();
+			d1.SetComponent(c);
+			d2.SetComponent(d1);
+			d2.Operation();
+			Console.ReadKey();
+		}
+	}
+
+	abstract class Component
+	{
+		public abstract void Operation();
+	}
+
+	class ConcreteComponent : Component
+	{
+		public override void Operation()
+		{
+			Console.WriteLine("ConcreteComponent.Operation()");
+		}
+	}
+
+	abstract class Decorator : Component
+	{
+		protected Component component;
+
+		public void SetComponent(Component component)
+		{
+			this.component = component;
+		}
+
+		public override void Operation()
+		{
+			if (component != null)
+			{
+				component.Operation();
+			}
+		}
+	}
+
+	class ConcreteDecoratorA : Decorator
+	{
+		public override void Operation()
+		{
+			base.Operation();
+			Console.WriteLine("ConcreteDecoratorA.Operation()");
+		}
+	}
+
+	class ConcreteDecoratorB : Decorator
+	{
+		public override void Operation()
+		{
+			base.Operation();
+			AddedBehavior();
+			Console.WriteLine("ConcreteDecoratorB.Operation()");
+		}
+		void AddedBehavior()
+		{ }
+	}
+}
