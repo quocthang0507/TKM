b23b2b219445f61703573a689659a909a3a8ab49
La Quốc Thắng <quocthang0507@gmail.com>
2019-11-30 09:44:05 +0700

    Update comments

--- Lab/Lab 2/BridgePattern/Program.cs
+++ Lab/Lab 2/BridgePattern/Program.cs
@@ -17,46 +17,79 @@ namespace BridgePattern
 		}
 	}
 
+	/// <summary>
+	/// Môn học
+	/// </summary>
 	abstract class Subject
 	{
+		/// <summary>
+		/// Đăng ký môn học
+		/// </summary>
+		/// <param name="name">Sinh viên đăng ký</param>
 		public abstract void Register(string name);
 	}
 
+	/// <summary>
+	/// Môn Công nghệ phần mềm
+	/// </summary>
 	class SoftwareEngineering : Subject
 	{
+		/// <summary>
+		/// Đăng ký môn học Công nghệ phần mềm
+		/// </summary>
+		/// <param name="name">Sinh viên đăng ký</param>
 		public override void Register(string name)
 		{
 			Console.WriteLine(name + " has just registered " + typeof(SoftwareEngineering).Name);
 		}
 	}
 
+	/// <summary>
+	/// Môn Thiết kế mẫu
+	/// </summary>
 	class DesignPattern : Subject
 	{
+		/// <summary>
+		/// Đăng ký môn học Thiết kế mẫu
+		/// </summary>
+		/// <param name="name">Sinh viên đăng ký</param>
 		public override void Register(string name)
 		{
 			Console.WriteLine(name + " has just registered " + typeof(DesignPattern).Name);
 		}
 	}
 
-	class StudentUniversity : Student
-	{
-		public StudentUniversity(string name)
-		{
-			this.Name = name;
-		}
-		public override void Operation()
-		{
-			subject.Register(this.Name);
-		}
-	}
-
+	/// <summary>
+	/// Sinh viên
+	/// </summary>
 	abstract class Student
 	{
 		protected Subject subject;
 		public string Name;
 		public Subject Subject { set { subject = value; } }
 
+		/// <summary>
+		/// Hành động tương tác với môn học
+		/// </summary>
 		public abstract void Operation();
+	}
 
+	/// <summary>
+	/// Sinh viên đại học
+	/// </summary>
+	class StudentUniversity : Student
+	{
+		public StudentUniversity(string name)
+		{
+			this.Name = name;
+		}
+
+		/// <summary>
+		/// Hành động tương tác với môn học
+		/// </summary>
+		public override void Operation()
+		{
+			subject.Register(this.Name);
+		}
 	}
 }
