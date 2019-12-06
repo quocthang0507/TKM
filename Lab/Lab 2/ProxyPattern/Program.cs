6100f52b9024fc5c0907b67d3281fb4ccfbcce6f
La Quốc Thắng <quocthang0507@gmail.com>
2019-12-06 20:44:18 +0700

    Update project

---
+++ Lab/Lab 2/ProxyPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TheTinDung proxy = new TheTinDung();
+			proxy.Request();
+			Console.ReadKey();
+		}
+	}
+
+	abstract class Tien
+	{
+		public abstract void Request();
+	}
+
+	class TienMat : Tien
+	{
+		public override void Request()
+		{
+			Console.WriteLine("Da thanh toan thanh cong");
+		}
+	}
+
+	class TheTinDung : Tien
+	{
+		private TienMat realSubject;
+
+		public TheTinDung()
+		{
+		}
+
+		public override void Request()
+		{
+			if (realSubject == null)
+				realSubject = new TienMat();
+			Console.WriteLine("Su dung the tin dung thay the tien mat");
+			realSubject.Request();
+		}
+	}
+}
