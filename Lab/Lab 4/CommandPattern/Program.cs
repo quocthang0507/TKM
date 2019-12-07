4420715c8861b10f745124167d100cc58ae3b26d
La Quốc Thắng <quocthang0507@gmail.com>
2019-12-07 07:22:45 +0700

    Clean Code

--- Lab/Lab 4/CommandPattern/Program.cs
+++ Lab/Lab 4/CommandPattern/Program.cs
@@ -10,6 +10,58 @@ namespace CommandPattern
 	{
 		static void Main(string[] args)
 		{
+			Receiver reveiver = new Receiver();
+			Command command = new ConcreteCommand(reveiver);
+			Invoker invoker = new Invoker();
+			invoker.SetCommand(command);
+			invoker.ExecuteCommand();
+			Console.ReadKey();
 		}
+	}
+
+	abstract class Command
+	{
+		protected Receiver receiver;
+
+		public Command(Receiver receiver)
+		{
+			this.receiver = receiver;
+		}
+
+		public abstract void Execute();
+	}
+
+	class ConcreteCommand : Command
+	{
+		public ConcreteCommand(Receiver receiver) : base(receiver) { }
+
+		public override void Execute()
+		{
+			receiver.Action();
+		}
+	}
+
+	class Receiver
+	{
+		public void Action()
+		{
+			Console.WriteLine("Called Receiver Action()");
+		}
+	}
+
+	class Invoker
+	{
+		private Command command;
+
+		public void ExecuteCommand()
+		{
+			command.Execute();
+		}
+
+		public void SetCommand(Command command)
+		{
+			this.command = command;
+		}
+
 	}
 }
